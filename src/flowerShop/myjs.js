96218ca078e4524bba725d5c7982db33d22aba9c
SCUTtanqi <674416265@qq.com>
2014-11-17 20:36:15 +0800

    1

--- src/flowerShop/myjs.js
+++ src/flowerShop/myjs.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+	$("#picture1").load(function(){
+		var can1 = document.getElementById("canvas1");
+		var pic1 = document.getElementById("picture1");
+		can1.width = "500px";
+		can1.height = "300px";
+		stackBlurImage('picture1','canvas1',50,false);
+	})
+	$("#picture2").load(function(){
+		stackBlurImage('picture2','canvas2',50,false);
+	})
 	$("#bottom1").click(function(){
 		$("#picture1").slideToggle("fast");
 	});
